0ce85801189c6bd4de7183c57745deb715ef4f75
Udit Kumar Mozumder <udit.mozumder@zohomail.in>
2025-06-16 13:41:06 +0530

    Fix:removed leftover merge markers

--- samlStrategy.js
+++ samlStrategy.js
@@ -1,21 +1,27 @@
 const passport = require('passport');
 const SamlStrategy = require('passport-saml').Strategy;
+require('dotenv').config();
 
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-passport.use(new SamlStrategy(
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+const samlStrategy = new SamlStrategy(
   {
     entryPoint: process.env.SAML_ENTRY_POINT,
     issuer: process.env.SAML_ISSUER,
     callbackUrl: process.env.SAML_CALLBACK_URL,
-    cert: process.env.SAML_CERT.replace(/\\n/g, '\n'),
+    cert: process.env.SAML_CERT,
+    identifierFormat: null
   },
-  (profile, done) => done(null, profile)
-));
+  function(profile, done) {
+    return done(null, profile);
+  }
+);
 
-<<<<<<< HEAD
+passport.use(samlStrategy);
 module.exports = passport;
-=======
-module.exports = passport;
->>>>>>> d1de502 (Step 4: Added SAML login routes and auth logic)
